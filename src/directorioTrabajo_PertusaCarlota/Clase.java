d808a2fcfc85f237ac36b58946fafb0b73ee70b0
unknown <DAM118@iesataulfo.local>
2024-04-16 16:42:35 +0200

    Carlota

---
+++ src/directorioTrabajo_PertusaCarlota/Clase.java
@@ -0,0 +1,16 @@
+package directorioTrabajo_PertusaCarlota;
+
+/**
+ * Esta clase contendra un constructor por defecto que muestra por pantalla un mensaje
+ * 
+ * @author Carlota Pertusa
+ */
+public class Clase 
+{
+	
+	public Clase() 
+	{
+        System.out.println("Soy un objeto de la clase y acabo de ser creado");
+    }
+	
+}
---
+++ src/directorioTrabajo_PertusaCarlota/Principal.java
@@ -0,0 +1,17 @@
+package directorioTrabajo_PertusaCarlota;
+
+/**
+ * En esta clase instanciaremos el objeto 'Clase'
+ * 
+ * @author Carlota Pertusa
+ */
+public class Principal {
+
+	public static void main(String[] args)
+	{
+		
+		 Clase objeto = new Clase();
+	    
+	}
+
+}

75a1315f1fa2baae0d73fa4570a9341a3d576ffc
unknown <DAM118@iesataulfo.local>
2024-04-16 15:46:03 +0200

    Primera fase proyecto (Clase1 y Principal creadas)

--- src/directorioTrabajo_PertusaCarlota/Principal.java
+++ src/directorioTrabajo_PertusaCarlota/Principal.java
@@ -2,9 +2,9 @@ package directorioTrabajo_PertusaCarlota;
 
 public class Principal {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args){
 
+		System.out.println(" crea un objeto de la clase “Clase”");
 	}
 
 }
